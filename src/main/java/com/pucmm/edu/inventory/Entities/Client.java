f98dbd1b281603719e375ca5bcbca2602bdb9a3b
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-06-05 19:19:34 -0400

    Equipment entity

---
+++ src/main/java/com/pucmm/edu/inventory/Entities/Client.java
@@ -0,0 +1,112 @@
+package com.pucmm.edu.inventory.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private String uuid;
+    private String firstName;
+    private String lastName;
+    private String id; // DNI or Cedula
+    private String phoneNumber;
+    private String email;
+    private String photo;
+    private boolean isActive;
+    @OneToMany
+    private Set<Role> role;
+
+    public Client(String firstName, String lastName, String id, String phoneNumber, String email, String photo, boolean isActive, Set<Role> role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.photo = photo;
+        this.isActive = isActive;
+        this.role = role;
+    }
+
+    public Client() {
+
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Set<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<Role> role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/pucmm/edu/inventory/Entities/Equipment.java
@@ -0,0 +1,87 @@
+package com.pucmm.edu.inventory.Entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Equipment implements Serializable {
+    @Id
+    @GeneratedValue
+    private String uuid;
+    private String name;
+    private long rate;
+    @OneToOne
+    private Category category;
+    @OneToOne
+    private Category subCategory;
+    private boolean isActive;
+    private int days;
+    public Equipment() {
+
+    }
+
+    public Equipment(String name, long rate, Category category, Category subCategory, boolean isActive, int days) {
+        this.name = name;
+        this.rate = rate;
+        this.category = category;
+        this.subCategory = subCategory;
+        this.isActive = isActive;
+        this.days = days;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getRate() {
+        return rate;
+    }
+
+    public void setRate(long rate) {
+        this.rate = rate;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Category getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(Category subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+}
