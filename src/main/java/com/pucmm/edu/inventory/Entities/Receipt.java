f434243fc2593770cd77b7e5ffcabfcf82e11681
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-06-05 20:06:52 -0400

    Creates Receipt

--- src/main/java/com/pucmm/edu/inventory/Entities/Receipt.java
+++ src/main/java/com/pucmm/edu/inventory/Entities/Receipt.java
@@ -1,37 +1,102 @@
 package com.pucmm.edu.inventory.Entities;
 
+
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
 
-public class Receipt {
+@Entity
+public class Receipt implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false)
-    private int uuid;
-
-    private String date;
-
-    private Date returnDate;
-
+    private String uuid;
+    private Date date;
+    private String returnDate;
     @OneToOne
     private  Client client;
-
-    @OneToMany(fetch = FetchType.EAGER)
-    private Set<EquipRental> equipRental;
-
+    @OneToMany
+    private Set<EquipmentRental> equipmentRental;
     private float cost;
-
-    public int getDiasRent() {
-        return diasRent;
-    }
-
-    public void setDiasRent(int diasRent) {
-        this.diasRent = diasRent;
-    }
-
-    private int diasRent;
-
+    private int days;
     private Boolean pending;
+
+
+    public Receipt() {
+    }
+
+    public Receipt(Date date, String returnDate, Client client, Set<EquipmentRental> equipmentRental, float cost, int days, Boolean pending) {
+        this.date = date;
+        this.returnDate = returnDate;
+        this.client = client;
+        this.equipmentRental = equipmentRental;
+        this.cost = cost;
+        this.days = days;
+        this.pending = pending;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Set<EquipmentRental> getEquipmentRental() {
+        return equipmentRental;
+    }
+
+    public void setEquipmentRental(Set<EquipmentRental> equipmentRental) {
+        this.equipmentRental = equipmentRental;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public Boolean getPending() {
+        return pending;
+    }
+
+    public void setPending(Boolean pending) {
+        this.pending = pending;
+    }
 }
