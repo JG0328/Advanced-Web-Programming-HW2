86d8cbe94d48c129439c5ca815dde8eafee16615
José Gabriel González <jgg032899@gmail.com>
2020-06-05 21:28:12 -0400

    Update MyUserDetailsService.java

--- src/main/java/com/pucmm/edu/inventory/Services/MyUserDetailsService.java
+++ src/main/java/com/pucmm/edu/inventory/Services/MyUserDetailsService.java
@@ -17,8 +17,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private RolesRepository rolesRepository;
