e767f507a16dc04b75a4159d35cad4d316556c4a
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-06-12 23:56:54 -0400

    Equipment mapping

--- src/main/java/com/pucmm/edu/inventory/Controllers/EquipmentController.java
+++ src/main/java/com/pucmm/edu/inventory/Controllers/EquipmentController.java
@@ -1,5 +1,7 @@
 package com.pucmm.edu.inventory.Controllers;
 
+import com.pucmm.edu.inventory.Entities.Equipment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import java.util.HashMap;
 
 @Controller
 public class EquipmentController {
+
     @RequestMapping(value = "/equipment", method = RequestMethod.GET)
     public ModelAndView main() {
         var params = new HashMap<String, Object>();
