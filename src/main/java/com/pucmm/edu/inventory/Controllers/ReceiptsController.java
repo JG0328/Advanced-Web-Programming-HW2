5c87716d8eec3caa5a5d70199dade6cab92df950
José Gabriel González <jgg032899@gmail.com>
2020-06-13 21:34:05 -0400

    Setting up ReceiptsController

--- src/main/java/com/pucmm/edu/inventory/Controllers/ReceiptsController.java
+++ src/main/java/com/pucmm/edu/inventory/Controllers/ReceiptsController.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.ParseException;
 import java.util.List;
 
 @Controller
@@ -27,12 +29,28 @@ public class ReceiptsController {
     @Autowired
     private ClientsServices clientsServices;
 
-    @RequestMapping(value = "/receipt", method = RequestMethod.GET)
-    public ModelAndView indexReceipts() {
+    @RequestMapping(value = "/rental", method = RequestMethod.GET)
+    public ModelAndView indexRentals() {
         ModelAndView model = new ModelAndView();
         List<Receipt> r = receiptsServices.receiptList(true);
         model.addObject("receipts", r);
+        model.setViewName("receipt/rental");
+        return model;
+    }
+
+    @RequestMapping(value = "/receipt", method = RequestMethod.GET)
+    public ModelAndView indexReceipts() {
+        ModelAndView model = new ModelAndView();
+        float c = 0;
+        List<Receipt> r = receiptsServices.receiptList(false);
+        model.addObject("receipts", r);
         model.setViewName("receipt/receipt");
+        return model;
+    }
+
+    @RequestMapping(value = "/receipt/create", method = RequestMethod.GET)
+    public ModelAndView createReceipt(@RequestParam("id") int id) throws ParseException {
+        ModelAndView model = new ModelAndView();
         return model;
     }
 }
